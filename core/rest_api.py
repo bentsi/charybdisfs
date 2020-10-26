a4a0a3eab12f17906dcc51a3d4338b5fcd98f4fa
shoshan.dagany <shoshan.dagany@scylladb.com>
2020-10-26 13:10:49 +0200

    connected and fail with deserialize Error :rest_api
    error notification:rest_api

--- core/rest_api.py
+++ core/rest_api.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import copy
+import json
 import uuid
 import cherrypy
 from core.configuration import Configuration
@@ -25,28 +26,31 @@ DEFAULT_PORT = 8080
 class Root(object):
 
     @staticmethod
-    def create_object_from_json_extract_classname(json):
-        cls = json.get('classname', None)
+    def create_object_from_json_extract_classname(jsonobj):
+        jsonloaded : dict = json.loads(jsonobj)
+        cls = jsonloaded.get('classname', None)
         if cls is None:
-            print(f'did not foud classname field in json')
-            return None
-        clsptr = core.faults.attrs(cls)
-        json.pop(cls)
+            print(f'did not found classname field in json')
+            return NameError('classname') ,None
+        print(cls)
+        clsptr = getattr(core.faults, cls)
+        jsonloaded.pop('classname')
         if clsptr is None:
             print(f'did not found class {cls}')
-            return None
+            return NameError('cls') ,None
         fault = None
         try:
-            fault = clsptr._deserialize()
+            fault = clsptr._deserialize(json.dumps(jsonloaded))
         except BaseException as ex:
-            print(f"exception {ex} when trying to deserialize {json} cls={cls}")
-        return fault
+            print(f"exception {ex} when trying to deserialize {jsonloaded} cls={cls}")
+            return ex, None
+        return None, fault
 
-    def add_fault(uuid, json):
-        faultobj = Root.create_object_from_json_extract_classname(json)
+    @staticmethod
+    def add_fault(uuid, faultobj):
         if faultobj is None:
-            print(f"fail creating obj from uuid={uuid} json={json}")
-            return 0
+            print(f"fail creating obj from uuid={uuid} obj={faultobj})")
+            return "0"
         Configuration.set_fault(uuid, faultobj)
         return uuid
 
@@ -60,25 +64,26 @@ class Root(object):
     def faults(self, fault_id=None):
         method = cherrypy.request.method
         params = cherrypy.request.params
-        json = None if method == 'DELETE' else cherrypy.request.json
+        jsonobj = None if method == 'DELETE' or method == 'GET' else cherrypy.request.json
         print(f'fault_id={fault_id}')
         print(f'method={method}')
         print(f'params={params}')
-        print(f'json={json}')
+        print(f'json={jsonobj}')
         if method == 'GET':
             if fault_id is None:
                 ids = {'faults ids': Configuration.get_all_faults_ids()}
                 return ids
             else:
-                return self.faults[fault_id]
+                return {'fault_id': 'fault_id', 'fault':Configuration.get_fault(fault_id)._serialize()}
         if method == 'POST' or method == 'CREATE' or method == 'PUT':
             fault_id = str(uuid.uuid4())
-            faultobj = Root.create_object_from_json_extract_classname(json)
-            if faultobj:
-                Configuration.set_fault(fault_id, faultobj)
-            else:
-                fault_id = "0"
+            ex, faultobj = Root.create_object_from_json_extract_classname(jsonobj)
+            fault_id= Root.add_fault(fault_id, faultobj)
+            if ex is not None:
+                cherrypy.response.status = 500
+                return {'fault_id': fault_id, 'exception': str(ex)}
             return {'fault_id': fault_id}
+
         if method == 'DELETE':
             removed_uuid = Configuration.remove_fault(fault_id)
             if removed_uuid:
@@ -91,7 +96,7 @@ class Root(object):
 def rest_start(port=DEFAULT_PORT):
     cherrypy.config.update({
         'server.socket_host': '0.0.0.0',
-        'server.socket_port': port,
+        'server.socket_port': port
     })
     cherrypy.quickstart(Root())
 
