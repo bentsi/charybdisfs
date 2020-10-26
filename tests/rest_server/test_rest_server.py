2ee08867a751282c792243f68ed8c8cf25c11b56
shoshan.dagany <shoshan.dagany@scylladb.com>
2020-10-26 10:44:20 +0200

    test_error_fault independent server :test_rest_server
    add deserialize direct test
    
    skip e2e server test

--- tests/rest_server/test_rest_server.py
+++ tests/rest_server/test_rest_server.py
@@ -15,24 +15,57 @@
 import pytest
 import requests
 import errno
+import json
 from core.rest_api import rest_start
 from core.faults import LatencyFault, ErrorFault, SysCall, Status
+import core
+
+# server bingup in other process
+#@pytest.fixture(scope="module")
+#def rest_server():
+#    return rest_start()
 
 
-@pytest.fixture(scope="module")
-def rest_server():
-    return rest_start()
+@pytest.mark.skip(reason="testing with server")
+def test_error_fault_deserialize_directly():
+    error_fault = ErrorFault(sys_call=SysCall(SysCall.WRITE), error_no=errno.ENOSPC, probability=100)
+    json_obj = error_fault._serialize()
+    jsonloaded = json.loads(json_obj)
+    cls = jsonloaded.get('classname', None)
+    if cls is None:
+        print(f'did not foud classname field in json')
+        return None
+    print(cls)
+    clsptr = getattr(core.faults, cls)
+    jsonloaded.pop('classname')
+    if 'fault_id' in jsonloaded:
+        jsonloaded.pop('fault_id')
+    if clsptr is None:
+        print(f'did not found class {cls}')
+        return None
+    fault = None
+    try:
+        fault = clsptr._deserialize(json.dumps(jsonloaded))
+    except BaseException as ex:
+        print(f"exception {ex} when trying to deserialize {jsonloaded} cls={cls}")
+        print("FIX EXCEPTION")
+    return fault
 
 
+@pytest.mark.skip(reason='first need pass on deserialize')
 def test_add_error_fault():
-    error_fault = ErrorFault(sys_call=SysCall.WRITE, probability=100, error_no=errno.ENOSPC)
+    error_fault = ErrorFault(sys_call=SysCall.WRITE, error_no=errno.ENOSPC, probability=100)
     s = requests.Session()
     base = 'http://127.0.0.1:8080/'
     resource = 'faults'
     try:
-        response = s.post(f'{base}{resource}', json=error_fault._serialize())
+        json_obj = error_fault._serialize()
+
+        response = s.post(f'{base}{resource}', json=json_obj)
         if response.status_code != 200:
-            raise ValueError(f"Failed to send fault ={errno}. Returned {response.json()}")
-        return response.json()
+            error_string = f"Failed to send fault ={errno}. Returned {response.json()}"
+            raise ValueError(error_string)
     except Exception as e:
         print(f"Exception {e}")
+        raise
+    assert(response.status_code == 200)
